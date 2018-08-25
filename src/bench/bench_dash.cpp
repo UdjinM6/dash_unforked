36f6503543e494030d19a17f479f4115063107f1
Wladimir J. van der Laan <laanwj@gmail.com>
2018-08-25 20:20:30 +0200

    Merge #13961: util: Replace boost::signals2 with std::function
    ddddce0e46e73d4ca369f2ce9696231cc579e1f9 util: Replace boost::signals2 with std::function (MarcoFalke)
    
    Pull request description:
    
      This removes the `#include <boost/signals2/signal.hpp>` from `util.h` (hopefully speeding up the build time and reducing the memory usage further after  #13634)
    
      The whole translation interface is replaced by a function `G_TRANSLATION_FUN` that is set to nullptr in units that don't need translation. (Thus only set in the gui)
    
    Tree-SHA512: 087c717358bbed8bdb409463e225239d667f1ced381abb10e7cd31a41dcdd2cebe20b43c2ee86f0f8e55d53301f75e963f07421a99a7ff4c0cad2c6a375c5ab1
    
    # Conflicts:
    #	src/bench/bench_dash.cpp
    #	src/qt/dash.cpp
    #	src/qt/splashscreen.cpp
    #	src/qt/transactiontablemodel.cpp
    #	src/test/test_dash.cpp
    #	src/util/system.h
    #	src/wallet/coinselection.cpp

--- src/bench/bench_dash.cpp
+++ src/bench/bench_dash.cpp
@@ -17,6 +17,8 @@
 
 #include <bls/bls.h>
 
+const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
+
 static const int64_t DEFAULT_BENCH_EVALUATIONS = 5;
 static const char* DEFAULT_BENCH_FILTER = ".*";
 static const char* DEFAULT_BENCH_SCALING = "1.0";
