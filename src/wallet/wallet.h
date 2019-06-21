2b71330216c62e9306a8941f665ec4879a28419d
MeshCollider <dobsonsa68@gmail.com>
2019-06-21 19:58:18 +1200

    Merge #16226: Move ismine to the wallet module
    e61de6306fd89fe9aae90253062e7b1b20343f8a Change ismine to take a CWallet instead of CKeyStore (Andrew Chow)
    7c611e20007bf5face34d33dffa26c8db67e29ec Move ismine to wallet module (Andrew Chow)
    
    Pull request description:
    
      `IsMine` isn't used outside of the wallet except for the tests. It also doesn't make sense to be outside of the wallet. This PR moves `IsMine` into the wallet module and for it to take a `CWallet` instead of `CKeyStore`. The test that used `IsMine` is also moved to the wallet tests.
    
      This is first [prerequisites](https://github.com/bitcoin-core/bitcoin-devwiki/wiki/Wallet-Class-Structure-Changes#ismine) for the wallet structure changes.
    
    ACKs for commit e61de6:
      MarcoFalke:
        re-ACK e61de6306f (only change is rebase with git auto-merge)
      meshcollider:
        Very light code review ACK https://github.com/bitcoin/bitcoin/pull/16226/commits/e61de6306fd89fe9aae90253062e7b1b20343f8a
    
    Tree-SHA512: 1cb4ad12652aef7922ab7460c6d413e8b9d1855dca78c0a286ae49d5c0765bc7996c55f262c742001d434eb9bd4215dc2cc7aae1b371ee1a82d46b32c17e6341

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -12,7 +12,7 @@
 #include <interfaces/handler.h>
 #include <policy/feerate.h>
 #include <saltedhasher.h>
-#include <script/ismine.h>
+#include <streams.h>
 #include <tinyformat.h>
 #include <ui_interface.h>
 #include <util/system.h>
@@ -21,6 +21,7 @@
 #include <wallet/coincontrol.h>
 #include <wallet/crypter.h>
 #include <wallet/coinselection.h>
+#include <wallet/ismine.h>
 #include <wallet/walletdb.h>
 #include <wallet/walletutil.h>
 
