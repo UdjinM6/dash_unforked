3475a136440dd85ce18e712f10d117454e5659ff
Samuel Dobson <dobsonsa68@gmail.com>
2019-11-23 09:26:13 +1300

    Merge #17237: wallet: LearnRelatedScripts only if KeepDestination
    3958295bc8a3787b66b0269190218a3764088f79 wallet: LearnRelatedScripts only if KeepDestination (João Barbosa)
    55295fba4cbff36e9a8c3fed9c38e82ebe3c48b7 wallet: Lock address type in ReserveDestination (João Barbosa)
    
    Pull request description:
    
      Only mutates the wallet if the reserved key is kept.
    
      First commit is a refactor that makes the address type a class member.
    
      The second commit moves `LearnRelatedScripts` from `GetReservedDestination` to `KeepDestination` to avoid an unnecessary call to `AddCScript` - which in turn prevents multiple entries of the same script in the wallet DB.
    
    ACKs for top commit:
      achow101:
        Re-ACK 3958295bc8a3787b66b0269190218a3764088f79
      Sjors:
        ACK 3958295bc8a3787b66b0269190218a3764088f79
      ryanofsky:
        Code review ACK 3958295bc8a3787b66b0269190218a3764088f79. I like this change. The new behavior makes more sense, and the change makes the code clearer, since the current LearnRelatedScripts call is hard to understand and explain. (Personally, I'd like it if this PR were merged before #17373 or that PR was rebased on top of this one so it would be less confusing.)
      meshcollider:
        utACK 3958295bc8a3787b66b0269190218a3764088f79
    
    Tree-SHA512: 49a5f4b022b28042ad37ea309b28378a3983cb904e234a25795b5a360356652e0f8e60f15e3e64d85094ea63af9be01812d90ccfc08ca4f1dd927fdd8566e33f

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -149,7 +149,7 @@ class ReserveDestination : public CReserveScript
 {
 protected:
     //! The wallet to reserve from
-    CWallet* pwallet;
+    CWallet* const pwallet;
     LegacyScriptPubKeyMan* m_spk_man{nullptr};
 
     //! The index of the address's key in the keypool
@@ -163,10 +163,9 @@ protected:
 
 public:
     //! Construct a ReserveDestination object. This does NOT reserve an address yet
-    explicit ReserveDestination(CWallet* pwalletIn)
-    {
-        pwallet = pwalletIn;
-    }
+    explicit ReserveDestination(CWallet* pwallet)
+      : pwallet(pwallet)
+      { }
 
     ReserveDestination(const ReserveDestination&) = delete;
     ReserveDestination& operator=(const ReserveDestination&) = delete;
