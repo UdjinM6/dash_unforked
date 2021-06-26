92e05078aea9fc35897e176371d0af58a263d6f0
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-06-26 15:33:16 +0530

    merge #18011: Replace current benchmarking framework with nanobench

--- src/bench/examples.cpp
+++ src/bench/examples.cpp
@@ -8,27 +8,27 @@
 
 // Sanity test: this should loop ten times, and
 // min/max/average should be close to 100ms.
-static void Sleep100ms(benchmark::State& state)
+static void Sleep100ms(benchmark::Bench& bench)
 {
-    while (state.KeepRunning()) {
+    bench.run([&] {
         MilliSleep(100);
-    }
+    });
 }
 
-BENCHMARK(Sleep100ms, 10);
+BENCHMARK(Sleep100ms);
 
 // Extremely fast-running benchmark:
 #include <math.h>
 
 volatile double sum = 0.0; // volatile, global so not optimized away
 
-static void Trig(benchmark::State& state)
+static void Trig(benchmark::Bench& bench)
 {
     double d = 0.01;
-    while (state.KeepRunning()) {
+    bench.run([&] {
         sum += sin(d);
         d += 0.000001;
-    }
+    });
 }
 
-BENCHMARK(Trig, 12 * 1000 * 1000);
+BENCHMARK(Trig);
