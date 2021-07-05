64f933df0d7eb5aaf84116dded9e47c863d00c47
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-07-06 02:17:01 +0300

    Always use C.UTF-8

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -1263,13 +1263,9 @@ void SetupEnvironment()
     }
 #endif
     // On most POSIX systems (e.g. Linux, but not BSD) the environment's locale
-    // may be invalid, in which case the "C" locale is used as fallback.
+    // may be invalid, just always use "C.UTF-8" locale.
 #if !defined(WIN32) && !defined(MAC_OSX) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
-    try {
-        std::locale(""); // Raises a runtime error if current locale is invalid
-    } catch (const std::runtime_error&) {
-        setenv("LC_ALL", "C", 1);
-    }
+    setenv("LC_ALL", "C.UTF-8", 1);
 #endif
     // The path locale is lazy initialized and to avoid deinitialization errors
     // in multithreading environments, it is set explicitly by the main thread.
