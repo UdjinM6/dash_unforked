d400a6f1594c585a8a8745dbc9af84d01865dc6d
UdjinM6 <UdjinM6@users.noreply.github.com>
2021-07-20 23:08:26 +0300

    fixes

--- test/functional/p2p_blocksonly.py
+++ test/functional/p2p_blocksonly.py
@@ -50,6 +50,7 @@ class P2PBlocksOnly(BitcoinTestFramework):
         txid = self.nodes[0].testmempoolaccept([sigtx])[0]['txid']
         with self.nodes[0].assert_debug_log(['received getdata for: tx {} peer=0'.format(txid)]):
             self.nodes[0].sendrawtransaction(sigtx)
+            self.bump_mocktime(60)
             self.nodes[0].p2p.wait_for_tx(txid)
             assert_equal(self.nodes[0].getmempoolinfo()['size'], 1)
 
