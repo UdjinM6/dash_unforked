c2913e13d94c2a3d946132d4b9e9c5c6ea2e3f7c
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-08-28 00:33:02 +0530

    merge #14954: Require python 3.5

--- test/functional/p2p_invalid_block.py
+++ test/functional/p2p_invalid_block.py
@@ -78,7 +78,7 @@ class InvalidBlockRequestTest(BitcoinTestFramework):
         block2.vtx.append(tx2)
         assert_equal(block2.hashMerkleRoot, block2.calc_merkle_root())
         assert_equal(orig_hash, block2.rehash())
-        assert(block2_orig.vtx != block2.vtx)
+        assert block2_orig.vtx != block2.vtx
 
         node.p2p.send_blocks_and_test([block2], node, success=False, request_block=False, reject_code=16, reject_reason=b'bad-txns-duplicate')
 
