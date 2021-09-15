Bitcoin-ABC/bitcoin-abc

Merge #14180: qa: Run all tests even if wallet is not compiled
Summary:
fac95398366f644911b58f1605e6bc37fb76782d qa: Run all tests even if wallet is not compiled (MarcoFalke)
faa669cbcd1fc799517b523b0f850e01b11bf40a qa: Premine to deterministic address with -disablewallet (MarcoFalke)

Pull request description:

  Currently the test_runner would exit if the wallet was not compiled into the Bitcoin Core executable. However, a lot of the tests run without the wallet just fine and there is no need to globally require the wallet to run the tests.

Tree-SHA512: 63177260aa29126fd20f0be217a82b10b62288ab846f96f1cbcc3bd2c52702437703475d91eae3f8d821a3149fc62b725a4c5b2a7b3657b67ffcbc81532a03bb

Backport of Core [[https://github.com/bitcoin/bitcoin/pull/14180/files | PR14180]]

Test Plan:
  cmake -GNinja .. -DBUILD_BITCOIN_WALLET=OFF
  ninja check
  ninja check-functional-extended

Reviewers: O1 Bitcoin ABC, #bitcoin_abc, deadalnix

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D5184

test/functional/test_framework/test_framework.py
@@ -50,6 +50,13 @@ TEST_EXIT_SKIPPED = 77
 TIMESTAMP_IN_THE_PAST = 1575158400
 
 
+class SkipTest(Exception):
+    """This exception is raised to skip a test"""
+
+    def __init__(self, message):
+        self.message = message
+
+
 class BitcoinTestFramework():
     """Base class for a bitcoin test script.
 
@@ -147,6 +154,7 @@ class BitcoinTestFramework():
             if self.options.usecli and not self.supports_cli:
                 raise SkipTest(
                     "--usecli specified but test does not support using CLI")
+            self.skip_test_if_missing_module()
             self.setup_chain()
             self.setup_network()
             self.import_deterministic_coinbase_privkeys()
@@ -214,6 +222,10 @@ class BitcoinTestFramework():
 
     def add_options(self, parser):
         """Override this method to add command-line options to the test"""
+        pass
+
+    def skip_test_if_missing_module(self):
+        """Override this method to skip a test if a module is not compiled"""
         pass
 
     def setup_chain(self):
@@ -517,31 +529,45 @@ class BitcoinTestFramework():
         for i in range(self.num_nodes):
             initialize_datadir(self.options.tmpdir, i)
 
+    def skip_if_no_py3_zmq(self):
+        """Attempt to import the zmq package and skip the test if the import fails."""
+        try:
+            import zmq  # noqa
+        except ImportError:
+            raise SkipTest("python3-zmq module not available.")
 
-class SkipTest(Exception):
-    """This exception is raised to skip a test"""
+    def skip_if_no_bitcoind_zmq(self):
+        """Skip the running test if bitcoind has not been compiled with zmq support."""
+        if not self.is_zmq_compiled():
+            raise SkipTest("bitcoind has not been built with zmq enabled.")
 
-    def __init__(self, message):
-        self.message = message
+    def skip_if_no_wallet(self):
+        """Skip the running test if wallet has not been compiled."""
+        if not self.is_wallet_compiled():
+            raise SkipTest("wallet has not been compiled.")
 
+    def skip_if_no_cli(self):
+        """Skip the running test if bitcoin-cli has not been compiled."""
+        if not self.is_cli_compiled():
+            raise SkipTest("bitcoin-cli has not been compiled.")
 
-def skip_if_no_py3_zmq():
-    """Attempt to import the zmq package and skip the test if the import fails."""
-    try:
-        import zmq  # noqa
-    except ImportError:
-        raise SkipTest("python3-zmq module not available.")
+    def is_cli_compiled(self):
+        """Checks whether bitcoin-cli was compiled."""
+        config = configparser.ConfigParser()
+        config.read_file(open(self.options.configfile, encoding='utf-8'))
 
+        return config["components"].getboolean("ENABLE_UTILS")
 
-def skip_if_no_bitcoind_zmq(test_instance):
-    """Skip the running test if bitcoind has not been compiled with zmq support."""
-    if not is_zmq_enabled(test_instance):
-        raise SkipTest("bitcoind has not been built with zmq enabled.")
+    def is_wallet_compiled(self):
+        """Checks whether the wallet module was compiled."""
+        config = configparser.ConfigParser()
+        config.read_file(open(self.options.configfile, encoding='utf-8'))
 
+        return config["components"].getboolean("ENABLE_WALLET")
 
-def is_zmq_enabled(test_instance):
-    """Checks whether zmq is enabled or not."""
-    config = configparser.ConfigParser()
-    config.read_file(open(test_instance.options.configfile, encoding='utf-8'))
+    def is_zmq_compiled(self):
+        """Checks whether the zmq module was compiled."""
+        config = configparser.ConfigParser()
+        config.read_file(open(self.options.configfile, encoding='utf-8'))
 
-    return config["components"].getboolean("ENABLE_ZMQ")
+        return config["components"].getboolean("ENABLE_ZMQ")

UdjinM6/dash_unforked

test/functional/test_framework/test_framework.py
@@ -65,6 +65,13 @@ TEST_EXIT_SKIPPED = 77
 
 GENESISTIME = 1417713337
 
+class SkipTest(Exception):
+    """This exception is raised to skip a test"""
+
+    def __init__(self, message):
+        self.message = message
+
+
 class BitcoinTestMetaClass(type):
     """Metaclass for BitcoinTestFramework.
 
@@ -210,6 +217,7 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         try:
             if self.options.usecli and not self.supports_cli:
                 raise SkipTest("--usecli specified but test does not support using CLI")
+            self.skip_test_if_missing_module()
             self.setup_chain()
             self.setup_network()
             self.import_deterministic_coinbase_privkeys()
@@ -286,6 +294,10 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
 
     def add_options(self, parser):
         """Override this method to add command-line options to the test"""
+        pass
+
+    def skip_test_if_missing_module(self):
+        """Override this method to skip a test if a module is not compiled"""
         pass
 
     def setup_chain(self):
@@ -1218,30 +1230,45 @@ class DashTestFramework(BitcoinTestFramework):
             return c >= count
         wait_until(test, timeout=timeout)
 
+    def skip_if_no_py3_zmq(self):
+        """Attempt to import the zmq package and skip the test if the import fails."""
+        try:
+            import zmq  # noqa
+        except ImportError:
+            raise SkipTest("python3-zmq module not available.")
 
-class SkipTest(Exception):
-    """This exception is raised to skip a test"""
-    def __init__(self, message):
-        self.message = message
+    def skip_if_no_bitcoind_zmq(self):
+        """Skip the running test if dashd has not been compiled with zmq support."""
+        if not self.is_zmq_compiled():
+            raise SkipTest("dashd has not been built with zmq enabled.")
 
+    def skip_if_no_wallet(self):
+        """Skip the running test if wallet has not been compiled."""
+        if not self.is_wallet_compiled():
+            raise SkipTest("wallet has not been compiled.")
 
-def skip_if_no_py3_zmq():
-    """Attempt to import the zmq package and skip the test if the import fails."""
-    try:
-        import zmq  # noqa
-    except ImportError:
-        raise SkipTest("python3-zmq module not available.")
+    def skip_if_no_cli(self):
+        """Skip the running test if bitcoin-cli has not been compiled."""
+        if not self.is_cli_compiled():
+            raise SkipTest("bitcoin-cli has not been compiled.")
 
+    def is_cli_compiled(self):
+        """Checks whether bitcoin-cli was compiled."""
+        config = configparser.ConfigParser()
+        config.read_file(open(self.options.configfile))
 
-def skip_if_no_bitcoind_zmq(test_instance):
-    """Skip the running test if dashd has not been compiled with zmq support."""
-    if not is_zmq_enabled(test_instance):
-        raise SkipTest("dashd has not been built with zmq enabled.")
+        return config["components"].getboolean("ENABLE_UTILS")
 
+    def is_wallet_compiled(self):
+        """Checks whether the wallet module was compiled."""
+        config = configparser.ConfigParser()
+        config.read_file(open(self.options.configfile))
 
-def is_zmq_enabled(test_instance):
-    """Checks whether zmq is enabled or not."""
-    config = configparser.ConfigParser()
-    config.read_file(open(test_instance.options.configfile))
+        return config["components"].getboolean("ENABLE_WALLET")
 
-    return config["components"].getboolean("ENABLE_ZMQ")
+    def is_zmq_compiled(self):
+        """Checks whether the zmq module was compiled."""
+        config = configparser.ConfigParser()
+        config.read_file(open(self.options.configfile))
+
+        return config["components"].getboolean("ENABLE_ZMQ")