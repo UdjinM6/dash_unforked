9722a3a1ccf0feb398aac42cae1b80136cdc4c17
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-10-01 15:52:43 +0530

    refactor: governance/governance-*[cpp/h] --> governance/*.[cpp/h]

--- src/governance/governance.h
+++ src/governance/governance.h
@@ -2,12 +2,12 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_GOVERNANCE_GOVERNANCE_H
-#define BITCOIN_GOVERNANCE_GOVERNANCE_H
+#ifndef BITCOIN_GOVERNANCE_H
+#define BITCOIN_GOVERNANCE_H
 
 #include <cachemap.h>
 #include <cachemultimap.h>
-#include <governance/governance-object.h>
+#include <governance/object.h>
 
 class CBloomFilter;
 class CBlockIndex;
@@ -382,4 +382,4 @@ private:
 
 bool AreSuperblocksEnabled();
 
-#endif // BITCOIN_GOVERNANCE_GOVERNANCE_H
+#endif // BITCOIN_GOVERNANCE_H
