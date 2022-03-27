3ebace02f5b1f5e7a2bcff133ecfb65de600b3e8
Vijay Das Manikpuri <2348066+vijaydasmp@users.noreply.github.com>
2022-03-27 11:46:39 +0530

    Merge #16226

--- src/wallet/test/ismine_tests.cpp
+++ src/wallet/test/ismine_tests.cpp
@@ -6,7 +6,7 @@
 #include <script/script.h>
 #include <script/script_error.h>
 #include <script/standard.h>
-#include <test/setup_common.h>
+#include <test/util/setup_common.h>
 #include <wallet/ismine.h>
 #include <wallet/wallet.h>
 
