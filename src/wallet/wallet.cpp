c6b51fa19126a765c5bee7de0f1fbaf1dce61c6a
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-05-07 21:50:42 +0530

    merge bitcoin#20016: 1 is a constant

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -265,8 +265,6 @@ WalletCreationStatus CreateWallet(interfaces::Chain& chain, const SecureString& 
     return WalletCreationStatus::SUCCESS;
 }
 
-const uint256 CWalletTx::ABANDON_HASH(UINT256_ONE());
-
 /** @defgroup mapWallet
  *
  * @{
