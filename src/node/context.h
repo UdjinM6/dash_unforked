c9b2fe5bff3e8194ea4fa59beb30c48b8487dead
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-06-10 16:25:53 +0530

    merge bitcoin#19277: Add Assert identity function

--- src/node/context.h
+++ src/node/context.h
@@ -51,10 +51,4 @@ struct NodeContext {
     ~NodeContext();
 };
 
-inline ChainstateManager& EnsureChainman(const NodeContext& node)
-{
-    assert(node.chainman);
-    return *node.chainman;
-}
-
 #endif // BITCOIN_NODE_CONTEXT_H
