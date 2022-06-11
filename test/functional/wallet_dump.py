6338a0d559ab05e8483064ac8c57ff4b28f48f7a
UdjinM6 <UdjinM6@users.noreply.github.com>
2022-06-12 01:06:56 +0300

    fix 18923

--- test/functional/wallet_dump.py
+++ test/functional/wallet_dump.py
@@ -86,6 +86,7 @@ class WalletDumpTest(BitcoinTestFramework):
         self.skip_if_no_wallet()
 
     def setup_network(self):
+        self.disable_mocktime()
         self.add_nodes(self.num_nodes, extra_args=self.extra_args)
         self.start_nodes()
 
