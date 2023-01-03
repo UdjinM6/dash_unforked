48459dbcc6e33b2c47701d036a9495d1281f9bf9
UdjinM6 <UdjinM6@users.noreply.github.com>
2023-01-03 23:18:12 +0300

    fix

--- src/node/context.h
+++ src/node/context.h
@@ -53,7 +53,7 @@ struct NodeContext {
     std::function<void()> rpc_interruption_point = [] {};
     //! Dash
     std::unique_ptr<LLMQContext> llmq_ctx;
-    std::unique_ptr<CCreditPoolManager> creditPoolManager{nullptr};
+    std::unique_ptr<CCreditPoolManager> creditPoolManager;
 
     std::unique_ptr<CEvoDB> evodb;
 
