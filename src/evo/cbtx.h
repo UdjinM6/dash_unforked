dashpay/dash

best CL sig in cbtx

src/evo/cbtx.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_EVO_CBTX_H
 #define BITCOIN_EVO_CBTX_H
 
+#include <bls/bls.h>
 #include <primitives/transaction.h>
 #include <univalue.h>
 
@@ -15,6 +16,7 @@ class CValidationState;
 
 namespace llmq {
 class CQuorumBlockProcessor;
+class CChainLocksHandler;
 }// namespace llmq
 
 // coinbase transaction
@@ -23,11 +25,14 @@ class CCbTx
 public:
     static constexpr auto SPECIALTX_TYPE = TRANSACTION_COINBASE;
     static constexpr uint16_t CURRENT_VERSION = 2;
+    static constexpr uint16_t CB_CL_SIG_VERSION = 3;
 
     uint16_t nVersion{CURRENT_VERSION};
     int32_t nHeight{0};
     uint256 merkleRootMNList;
     uint256 merkleRootQuorums;
+    uint32_t bestCLHeightDiff;
+    CBLSSignature bestCLSignature;
 
     SERIALIZE_METHODS(CCbTx, obj)
     {
@@ -35,6 +40,10 @@ public:
 
         if (obj.nVersion >= 2) {
             READWRITE(obj.merkleRootQuorums);
+            if (obj.nVersion >= CB_CL_SIG_VERSION) {
+                READWRITE(COMPACTSIZE(obj.bestCLHeightDiff));
+                READWRITE(obj.bestCLSignature);
+            }
         }
     }
 
@@ -49,6 +58,10 @@ public:
         obj.pushKV("merkleRootMNList", merkleRootMNList.ToString());
         if (nVersion >= 2) {
             obj.pushKV("merkleRootQuorums", merkleRootQuorums.ToString());
+            if (nVersion >= CB_CL_SIG_VERSION) {
+                obj.pushKV("bestCLHeightDiff", static_cast<int>(bestCLHeightDiff));
+                obj.pushKV("bestCLSignature", bestCLSignature.ToString());
+            }
         }
     }
 };
@@ -58,5 +71,8 @@ bool CheckCbTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidatio
 bool CheckCbTxMerkleRoots(const CBlock& block, const CBlockIndex* pindex, const llmq::CQuorumBlockProcessor& quorum_block_processor, CValidationState& state, const CCoinsViewCache& view);
 bool CalcCbTxMerkleRootMNList(const CBlock& block, const CBlockIndex* pindexPrev, uint256& merkleRootRet, CValidationState& state, const CCoinsViewCache& view);
 bool CalcCbTxMerkleRootQuorums(const CBlock& block, const CBlockIndex* pindexPrev, const llmq::CQuorumBlockProcessor& quorum_block_processor, uint256& merkleRootRet, CValidationState& state);
+bool CheckCbTxBestChainlock(const CBlock& block, const CBlockIndex* pindexPrev, const llmq::CChainLocksHandler& chainlock_handler, CValidationState& state);
+
+bool EmplaceBestChainlock(const llmq::CChainLocksHandler& chainlock_handler, const int nHeight, uint32_t& bestCLHeightDiff, CBLSSignature& bestCLSignature);
 
 #endif // BITCOIN_EVO_CBTX_H

UdjinM6/dash_unforked

src/evo/cbtx.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_EVO_CBTX_H
 #define BITCOIN_EVO_CBTX_H
 
+#include <bls/bls.h>
 #include <primitives/transaction.h>
 #include <univalue.h>
 
@@ -16,6 +17,7 @@ class TxValidationState;
 
 namespace llmq {
 class CQuorumBlockProcessor;
+class CChainLocksHandler;
 }// namespace llmq
 
 // coinbase transaction
@@ -24,11 +26,14 @@ class CCbTx
 public:
     static constexpr auto SPECIALTX_TYPE = TRANSACTION_COINBASE;
     static constexpr uint16_t CURRENT_VERSION = 2;
+    static constexpr uint16_t CB_CL_SIG_VERSION = 3;
 
     uint16_t nVersion{CURRENT_VERSION};
     int32_t nHeight{0};
     uint256 merkleRootMNList;
     uint256 merkleRootQuorums;
+    uint32_t bestCLHeightDiff;
+    CBLSSignature bestCLSignature;
 
     SERIALIZE_METHODS(CCbTx, obj)
     {
@@ -36,6 +41,10 @@ public:
 
         if (obj.nVersion >= 2) {
             READWRITE(obj.merkleRootQuorums);
+            if (obj.nVersion >= CB_CL_SIG_VERSION) {
+                READWRITE(COMPACTSIZE(obj.bestCLHeightDiff));
+                READWRITE(obj.bestCLSignature);
+            }
         }
     }
 
@@ -50,6 +59,10 @@ public:
         obj.pushKV("merkleRootMNList", merkleRootMNList.ToString());
         if (nVersion >= 2) {
             obj.pushKV("merkleRootQuorums", merkleRootQuorums.ToString());
+            if (nVersion >= CB_CL_SIG_VERSION) {
+                obj.pushKV("bestCLHeightDiff", static_cast<int>(bestCLHeightDiff));
+                obj.pushKV("bestCLSignature", bestCLSignature.ToString());
+            }
         }
     }
 };
@@ -59,5 +72,8 @@ bool CheckCbTx(const CTransaction& tx, const CBlockIndex* pindexPrev, TxValidati
 bool CheckCbTxMerkleRoots(const CBlock& block, const CBlockIndex* pindex, const llmq::CQuorumBlockProcessor& quorum_block_processor, BlockValidationState& state, const CCoinsViewCache& view);
 bool CalcCbTxMerkleRootMNList(const CBlock& block, const CBlockIndex* pindexPrev, uint256& merkleRootRet, BlockValidationState& state, const CCoinsViewCache& view);
 bool CalcCbTxMerkleRootQuorums(const CBlock& block, const CBlockIndex* pindexPrev, const llmq::CQuorumBlockProcessor& quorum_block_processor, uint256& merkleRootRet, BlockValidationState& state);
+bool CheckCbTxBestChainlock(const CBlock& block, const CBlockIndex* pindexPrev, const llmq::CChainLocksHandler& chainlock_handler, BlockValidationState& state);
+
+bool EmplaceBestChainlock(const llmq::CChainLocksHandler& chainlock_handler, const int nHeight, uint32_t& bestCLHeightDiff, CBLSSignature& bestCLSignature);
 
 #endif // BITCOIN_EVO_CBTX_H